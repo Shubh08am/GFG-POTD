8be62cf4e224c745d3420e343619eb39008044c4
Shubham Yadav <shubh08am@gmail.com>
2023-03-03 08:11:39 +0530

    Create Cutting Rectangles.cpp

---
+++ Cutting Rectangles.cpp
@@ -0,0 +1,7 @@
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        return {(L / __gcd(L, B)) * (B / __gcd(L, B)) , __gcd(L, B)};
+    }
+};
